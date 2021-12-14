013384c2c534ebd4fbf0fde8f4ed158c0a543a37
Luis Felipe Zaguini <luisfelipezaguini@gmail.com>
2021-12-13 22:27:03 -0300

    clone git repository

--- src/createSandbox.ts
+++ src/createSandbox.ts
@@ -1,12 +1,32 @@
 import path from 'node:path'
+import git from 'simple-git'
+import ora from 'ora'
 
 const getFallbackDirectoryName = (repositoryUrl: string) => {
   return path.parse(repositoryUrl).name
 }
 
-export const createSandbox = (
+const createSpinner = () => {
+  const spinner = ora()
+
+  return {
+    start: (text: string) => {
+      spinner.text = text
+      spinner.start()
+    },
+    succeed: () => spinner.succeed(),
+    stop: () => spinner.stop(),
+  }
+}
+
+export const createSandbox = async (
   repositoryUrl: string,
   directoryName = getFallbackDirectoryName(repositoryUrl)
 ) => {
-  console.log({ repositoryUrl, directoryName })
+  const spinner = createSpinner()
+  const sourceDirectory = path.resolve(process.cwd(), directoryName)
+
+  spinner.start('Cloning repository...')
+  await git().clone(repositoryUrl, sourceDirectory)
+  spinner.stop()
 }
